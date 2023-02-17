58077ee032261674dfae0f1a8838f486413c8b83
alalan55 <alla.ao4975@gmail.com>
2023-02-17 20:16:06 -0300

    Criação inicial da tela home

--- components/home/template.vue
+++ components/home/template.vue
@@ -1,15 +1,23 @@
 <script setup></script>
 <template>
   <section class="wrapper">
-    <!-- <img src="/images/cover-bg.jpg" alt="Background" class="wrapper__image" /> -->
+    <div class="wrapper__logo">
+      <span>LOGO.</span>
+    </div>
     <div class="wrapper__title">
-
-    <h1>Trindart studio</h1>
+      <span >Lorem ipsum at dolor met.</span>
+      <h1>Trindart studio</h1>
+    </div>
+    <div class="wrapper--action">
+      <div class="wrapper--action__button">
+        <span> Começar ↪ </span>
+      </div>
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
+@import "@/assets/main.scss";
 .wrapper {
   height: 100vh;
   background: url("images/cover-bg.jpg") no-repeat;
@@ -19,31 +27,81 @@
   display: flex;
   align-items: center;
   justify-content: center;
-  // display: grid;
-  // grid-template-areas: "stack";
-  // place-items: center;
+  flex-direction: column;
+  color: #ffffff;
 
-  // & > * {
-  //   grid-area: stack;
-  // }
-
-  // &__image {
-  //   width: 100%;
-  //   height: 100%;
-  //   object-fit: cover;
-  // }
+  &__logo {
+    height: 70px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    padding: $t-s-1;
+    span {
+      font-weight: 900;
+    }
+  }
   &__title {
     text-align: center;
+    flex: 3 1 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 
-    h1{
-
+    h1 {
       font-size: 8em;
       color: white;
       font-weight: 800;
-      transition: .3s ease-in-out;
+      transition: 0.3s ease-in-out;
 
-      @media(max-width:750px){
+      @media (max-width: 750px) {
         font-size: 4em;
+      }
+    }
+  }
+  &--action {
+    padding: $t-s-2;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    &__button {
+      cursor: pointer;
+      display: block;
+      width: auto;
+      width: 150px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: $t-s-1;
+      background-image: linear-gradient(144deg, #af40ff, #5b42f3 50%, #00ddeb);
+      border-radius: 15px;
+      animation: move 6s infinite;
+      position: relative;
+
+      &:hover {
+        outline: 0;
+      }
+
+      span {
+        font-size: 1.1em;
+        font-weight: 600;
+      }
+    }
+
+    @keyframes move {
+      0% {
+        top: 0px;
+      }
+      25% {
+        top: 20px;
+      }
+      // 75% {
+      //   top: 20px;
+      // }
+      100% {
+        top: 0px;
       }
     }
   }
