d3716b7a28d8362986368bd173c1fb0ff9a67deb
alalan55 <alla.ao4975@gmail.com>
2023-02-17 20:28:27 -0300

    Inicia criação do layout base

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper">
+    <h1>Layout base</h1>
+    <div class="wrapper__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
