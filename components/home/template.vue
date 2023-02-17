ed7fc52535b3206b641419dcfb5fde79885c1e70
alalan55 <alla.ao4975@gmail.com>
2023-02-17 19:38:18 -0300

    Criação inicial da tela home

--- components/home/template.vue
+++ components/home/template.vue
@@ -1,8 +1,51 @@
 <script setup></script>
 <template>
-  <div>
-    <h1>Home template</h1>
-  </div>
+  <section class="wrapper">
+    <!-- <img src="/images/cover-bg.jpg" alt="Background" class="wrapper__image" /> -->
+    <div class="wrapper__title">
+
+    <h1>Trindart studio</h1>
+    </div>
+  </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+  background: url("images/cover-bg.jpg") no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  // display: grid;
+  // grid-template-areas: "stack";
+  // place-items: center;
+
+  // & > * {
+  //   grid-area: stack;
+  // }
+
+  // &__image {
+  //   width: 100%;
+  //   height: 100%;
+  //   object-fit: cover;
+  // }
+  &__title {
+    text-align: center;
+
+    h1{
+
+      font-size: 8em;
+      color: white;
+      font-weight: 800;
+      transition: .3s ease-in-out;
+
+      @media(max-width:750px){
+        font-size: 4em;
+      }
+    }
+  }
+}
+</style>
