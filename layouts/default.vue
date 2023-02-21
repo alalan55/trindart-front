751170480eed79865c5996c3b6ad3a3627d94450
alalan55 <alla.ao4975@gmail.com>
2023-02-21 11:56:39 -0300

    Remoção de comentário

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,25 +7,17 @@
       <slot />
     </div>
     <div class="wrapper__footer">
-      <LayoutFooter/>
+      <LayoutFooter />
     </div>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/main.scss";
 
 .wrapper {
-  &__nav {
-    // height: $t-nav-bar; BOTANDO ESSE VALOR, PASSO AS INFORMAÇÕES PARA O TAMANHO DA BARRA DE NAVEGAÇÃO
-  }
-  &__content {
-    // background: red;
-    // height: calc(300vh - $t-nav-bar);
-  }
 }
 </style>
