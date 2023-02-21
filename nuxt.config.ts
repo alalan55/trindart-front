c15375668eeeaa8407e06d466796ce010f0c34a1
alalan55 <alla.ao4975@gmail.com>
2023-02-21 20:57:21 -0300

    Inclusão de biblioteca

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  css: ["@/assets/main.scss", "animate.css/animate.min.css"],
+  css: [
+    "@/assets/main.scss",
+    "animate.css/animate.min.css",
+    "vue-page-transition/dist/vue-page-transition.css",
+  ],
   plugins: [
     {
       src: "@/plugins/autoanimate.js",
       mode: "client",
     },
+    // '@/plugins/page-transition.js',
+    // {
+    //   src: "@/plugins/page-transition.js",
+    //   mode: "client",
+    // },
   ],
   modules: ["@vueuse/nuxt"],
   // app: {
